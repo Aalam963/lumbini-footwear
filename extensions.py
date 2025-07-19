a66f9924d860705395436e7d003350fea453644e
Aalam <oniseconds@gmail.com>
2025-07-19 16:24:31 +0545

    Fix merge conflict in extensions.py

--- extensions.py
+++ extensions.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-=======
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
->>>>>>> 17b7de751f5ec5f58a26b58b22ddcceb1adbbc18
